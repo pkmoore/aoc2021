fd25fb86989f6e9be9d8fab854271de3e2201f22
Preston <preston@preston.dev>
2021-12-03 22:30:22 -0500

    D1P2: New Impl

--- src/d1p2.rs
+++ src/d1p2.rs
@@ -9,10 +9,11 @@ fn main() -> std::io::Result<()> {
         .lines()
         .map(|x| x.unwrap().trim().parse::<u64>().unwrap())
         .collect::<Vec<u64>>();
-    let sums = data.windows(3).map(|x| x.iter().sum::<u64>())
-                              .collect::<Vec<u64>>();
-    let increases = sums.windows(2)
-                        .filter(|x| x[1] > x[0]).count();
+    let sums = data
+        .windows(3)
+        .map(|x| x.iter().sum::<u64>())
+        .collect::<Vec<u64>>();
+    let increases = sums.windows(2).filter(|x| x[1] > x[0]).count();
     println!("Day 1 Part 2 Answer: {}", increases);
     Ok(())
 }
