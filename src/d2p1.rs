15a5d04fe675f6f90f0287cb573a48ba411c21d6
Preston <preston@preston.dev>
2021-12-09 23:07:09 -0500

    D2P1: Initial Implementation

---
+++ src/d2p1.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./data/day2.txt")?;
+    let reader = BufReader::new(file);
+    let data = reader
+        .lines()
+        .map(|x| {
+            x.unwrap()
+                .split(" ")
+                .map(|y| y.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect::<Vec<Vec<String>>>();
+
+    let mut x_coord = 0;
+    let mut y_coord = 0;
+    for row in data {
+        let direction = &row[0];
+        let amount = row[1].parse::<u64>().unwrap();
+        match direction.as_str() {
+            "forward" => x_coord += amount,
+            "up" => y_coord -= amount,
+            "down" => y_coord += amount,
+            _ => panic!("Bad direction"),
+        }
+    }
+    println!("Result: {}", x_coord * y_coord);
+    Ok(())
+}
