2a86108ec363f887b1926b981b974e4e994ba2ef
Preston <preston@preston.dev>
2022-08-21 14:22:09 -0400

    D6P1: Initial Implementation

---
+++ src/d6p1.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./data/day6.txt")?;
+    let mut reader = BufReader::new(file);
+    let mut data = String::new();
+    reader.read_to_string(&mut data)?;
+    let mut fish = data.split(",").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+
+    for _day in 0..80 {
+        tick(&mut fish);
+    }
+
+    println!("D6P1 Result: {}", fish.len());
+    
+    Ok(())
+}
+
+fn tick(fish: &mut Vec<u32>) {
+    let mut new_fish_count: usize = 0;
+    for f in fish.iter_mut() {
+        if *f == 0 {
+            *f = 6;
+            new_fish_count += 1;
+        } else {
+            *f -= 1;
+        }
+    }
+    fish.extend_from_slice(&vec![8; new_fish_count]);
+}
